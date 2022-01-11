ef9a8c720956fc1b38ae0839c80b11ab681ae818
Dominik Roth <dominik@roth.systems>
2022-01-11 13:07:34 +0100

    feat(Crowdloan Tests Implementation):

--- src/tests/tx/crowdloanRewards/txCrowdloanRewardsTests.ts
+++ src/tests/tx/crowdloanRewards/txCrowdloanRewardsTests.ts
@@ -2,7 +2,26 @@
 import { ApiPromise } from '@polkadot/api';
 import { expect } from 'chai';
 import R from 'ramda';
+import {Promise} from 'bluebird';
 
+Promise.config({
+  // Enable warnings
+  warnings: true,
+  // Enable long stack traces
+  longStackTraces: true,
+  // Enable cancellation
+  cancellation: true,
+  // Enable monitoring
+  monitoring: true,
+  // Enable async hooks
+  asyncHooks: true,
+});
+
+process.on("promiseFulfilled", function(promise, child) {
+  // promise - The parent promise the child was chained from
+  // child - The created child promise.
+  console.debug();
+});
 
 export class TxCrowdloanRewardsTests {
   /**
@@ -17,14 +36,18 @@ export class TxCrowdloanRewardsTests {
   public static runTxCrowdloanRewardsTests() {
     describe('tx.crowdloanRewards Tests', function () {
       this.timeout(0);
-      it('tx.crowdloanRewards.populate', async function (done) {
-        await TxCrowdloanRewardsTests.txCrowdloanRewardsPopulateTest(done);
+      it('tx.crowdloanRewards.populate', async function () {
+        await TxCrowdloanRewardsTests.txCrowdloanRewardsPopulateTest().finally(function() {
+          console.debug("YAY");
+        });
+        console.debug();
       });
 
+      /* ToDo (D. Roth): Re- Implement using the way of the populate function.
       it('tx.crowdloanRewards.initialize', async function (done) {
         await TxCrowdloanRewardsTests.txCrowdloanRewardsInitializeTest(done);
         done();
-      });
+      });*/
     });
   }
 
@@ -52,7 +75,7 @@ export class TxCrowdloanRewardsTests {
   /**
    * 
    */
-  private static async txCrowdloanRewardsPopulateTest(done) {
+  private static async txCrowdloanRewardsPopulateTest() {
     // ToDo (D. Roth): Pass api and keyring instead of directly reading from global.
     const sudoKey = global.walletAlice;
     const vesting48weeks = 100800;
@@ -62,15 +85,45 @@ export class TxCrowdloanRewardsTests {
         n * 1_000_000_000_000,
         vesting48weeks
       ], n + 1], 1);
-    const populateHash = await global.api.tx.sudo.sudo(
-        global.api.tx.crowdloanRewards.populate(accounts)
-      ).signAndSend(sudoKey, { nonce: -1 }, ({ events=[], status }) => {
-        console.debug('txCrowdloanRewardsPopulateTest: Transaction status:', status.type);
-        if (status.isFinalized) {
-          console.debug('txCrowdloanRewardsPopulateTest: Finalized Transaction status:', status.type);
-          done();
-        }
-    });
+    try {
+      return new Promise(function (resolve, reject) {
+        global.api.tx.sudo.sudo(
+          global.api.tx.crowdloanRewards.populate(accounts)
+        ).signAndSend(sudoKey, { nonce: -1 }, ({ events=[], status }) => {
+          console.debug('txCrowdloanRewardsPopulateTest: Transaction status:', status.type);
+          if (status.isFinalized) {
+            console.debug('txCrowdloanRewardsPopulateTest: Finalized Transaction status:', status.type);
+
+            // ToDo (D. Roth): Call resolve when system.extrinsicSuccess
+            events
+            // find/filter for failed events
+            .filter(({ event }) =>
+              global.api.events.system.ExtrinsicFailed.is(event)
+            )
+            // we know that data for system.ExtrinsicFailed is
+            // (DispatchError, DispatchInfo)
+            .forEach(({ event: { data: [error, info] } }) => {
+              if (error.isModule) {
+                // for module errors, we have the section indexed, lookup
+                const decoded = global.api.registry.findMetaError(error.asModule);
+                const { docs, method, section } = decoded;
+
+                console.log(`${section}.${method}: ${docs.join(' ')}`);
+                throw new Error('txCrowdloanRewardsPopulateTest: ExtrinsicFailed!');
+              } else {
+                // Other, CannotLookup, BadOrigin, no extra info
+                console.log(error.toString());
+                throw new Error('txCrowdloanRewardsPopulateTest: ExtrinsicFailed!');
+              }
+            });
+            resolve();
+          }
+        });
+      });
+    }catch (exc) {
+      console.error(exc);
+    }
+    
     // ToDo (D. Roth): Add checks
     
   }
